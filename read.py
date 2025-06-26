8ec8eb89628ce87775eb0d69d99120331e71157f
houssainiS <houssaini.slimen@gmail.com>
2025-06-26 14:36:26 +0100

    resize video

--- read.py
+++ read.py
@@ -1,9 +1,9 @@
 import cv2 as cv
 
 #capturing images
-img =cv.imread('photos/cat.jpg') 
+# img =cv.imread('photos/cat.jpg') 
 
-cv.imshow('Cat', img)
+# cv.imshow('Cat', img)
 
 def rescaleFrame(frame, scale=0.75):
     width = int(frame.shape[1] * scale)
@@ -12,20 +12,24 @@ def rescaleFrame(frame, scale=0.75):
 
     return cv.resize(frame , dimensions , interpolation=cv.INTER_AREA)
 
-cv.waitKey(0)
+# cv.waitKey(0)
 
 
 
 
 #capturing videos
 
-#capture = cv.VideoCapture(0) #capturing facecam
-# capture = cv.VideoCapture('videos/Presentation asset.mp4') #capturing a saved video
+capture = cv.VideoCapture(0) #capturing facecam
+capture = cv.VideoCapture('videos/Presentation asset.mp4') #capturing a saved video
 
-# while True:
-#     isTrue, frame = capture.read()
-#     cv.imshow('Video', frame)
-#     if cv.waitKey(20) & 0xFF==ord('d'):
-#         break
-# capture.release()
-# cv.destroyAllWindows()
+while True:
+    isTrue, frame = capture.read()
+
+    frame_resized = rescaleFrame(frame)
+    cv.imshow('Video', frame)
+    cv.imshow('Video Resized', frame_resized)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+capture.release()
+cv.destroyAllWindows()
